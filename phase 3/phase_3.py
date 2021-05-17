1323096eab86cbb8d40c86f5578173ba874ece54
Balaji Padamwar <balaji.padamwar18@vit.edu>
2021-05-17 22:12:32 +0530

    simulation fn

--- phase 3/phase_3.py
+++ phase 3/phase_3.py
@@ -1,5 +1,5 @@
 def begin():
-    global m, IR, IC, R, C, SI, PI, TI, PTR, used_frames, memory, opfile, input_buffer, data_index, pd_error, gd_error, supervisory_storage, drum, TS, TSC, CH, ebq, ifbq, ofbq, rq, ioq, lq, tq, IOI, CHT, CH
+    global m,ch1,ch2,ch3, IR, IC, R, C, SI, PI, TI, PTR, used_frames, memory, opfile, input_buffer, data_index, pd_error, gd_error, supervisory_storage, drum, TS, TSC, CH, ebq, ifbq, ofbq, rq, ioq, lq, tq, IOI, CHT, CH
     m = 0
     IR = [0 for i in range(4)]
     IC = [0 for i in range(2)]
@@ -9,7 +9,9 @@ def begin():
     PI = 0
     TI = 0
     TS = 0
-    TSC = 0
+    ch1 = 5   #needs to be checked
+    ch2 = 2   #needs to be checked
+    ch3 = 3   #needs to be checked
     CH = [False for i in range(3)]  # channel flags
     CHT = [0 for i in range(3)]  # channel timers for all 3 channels
     ebq = []  # empty buffer queue
@@ -33,22 +35,73 @@ def begin():
     IOI = 0
     start_channel(1)
 
+class PCB:
+    def __init__(self, job_id, ttl, tll, ttc, llc,tsc):
+        self.job_id = job_id
+        self.TTL = ttl   # total time limit
+        self.TTC = ttc   # total time counter
+        self.TLL = tll   # total line limit
+        self.LLC = llc   # line limit counter
+        self.TSC = tsc   #time slice counter
+
+    #
+    #need to store count for data card and program card
+    #
+    #starting address of data card and program card
+    #
+    #starting address of op line
+    def incrementLLC(self):
+        self.LLC = self.LLC + 1
+
+    def incrementTTC(self):
+        self.TTC = self.TTC + 1
+
+    def incrementTSC(self):
+        self.TSC = self.TSC + 1
+
 
 def start_channel(i):
     global IOI, CH, CHT
     if(i == 1):
-        IOI += 1
+        IOI -= 1
         CH[0] = True
         CHT[0] = 0
     elif(i == 2):
-        IOI += 2
+        IOI -= 2
         CH[1] = True
         CHT[1] = 0
 
     elif(i == 3):
-        IOI += 4
+        IOI -= 4
         CH[2] = True
         CHT[2] = 0
+
+def simulation():
+    #
+    #obj needs to be changed later with the PCB object
+    obj=PCB()
+    obj.incrementTTC()
+    global TI,TS,CH,ch1,ch2,ch3,IOI
+    if(obj.TTC==obj.TTL):
+        TI=2
+        master_mode()
+    
+    obj.incrementTSC()
+    if(obj.TSC==TS):
+        TI=1
+        master_mode()
+
+    for i in range(3):
+        if(CH[i]):
+            CHT[i] += 1
+            if(i==0 and CHT[i]==ch1):
+                IOI += 1
+            elif(i==1 and CHT[i]==ch2):
+                IOI += 2
+            elif(i==2 and CHT[i]==ch3):
+                IOI += 4
+            master_mode()
+    
 
 
 if __name__ == '__main__':
